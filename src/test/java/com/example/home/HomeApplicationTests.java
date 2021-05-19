6bdee33a7bede4a69e846896fbdadb1c407b3ff7
huzhihui <huzhihui_c@qq.com>
2021-05-19 17:06:08 +0800

    'init'

---
+++ src/test/java/com/example/home/HomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
